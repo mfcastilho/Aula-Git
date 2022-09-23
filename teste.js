e90186c334b85e7d5e96b24a1e9acc760062f255
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-09-23 18:50:01 -0300

    Adicionando sobre nome e idade

--- teste.js
+++ teste.js
@@ -2,9 +2,18 @@ const msg = "Hello World!";
 console.log(msg);
 
 const nome = "Mario Frederico";
+<<<<<<< Updated upstream
 console.log(nome);
 
 
 
 const profissao = "programador";
 console.log(`Profissão:${profissao}`);
+=======
+
+const sobrenome = "de Castilho";
+console.log(`${nome} ${sobrenome}`);
+
+const idade = 39;
+console.log(`${idade} anos`);
+>>>>>>> Stashed changes
